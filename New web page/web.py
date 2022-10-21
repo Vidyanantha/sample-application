dc6694b76f48aff336a981821511f50a41c18954
sathyaselvan <sathyaselvan2000@gmail.com>
2022-10-21 08:00:22 -0700

    adding features to enable and disable

--- New web page/web.py
+++ New web page/web.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 app=Flask(__name__)
+
+edit_1=None
 
 @app.route("/")
 def Homepage():
@@ -7,11 +9,31 @@ def Homepage():
 
 @app.route("/profile" , methods=["GET","POST"])
 def edit():
-    username=request.form["User_Name"]
-    email=request.form["Email"]
-    date=request.form["date"]
-    number=request.form["number"]
-    return render_template("site1.html", name=username,email=email,date=date,number=number)
+    print(edit_1)
+    # username=request.form["User_Name"]
+    # email=request.form["Email"]
+    # date=request.form["date"]
+    # number=request.form["number"]
+    username="Sakthi"
+    email="Sakthi@gmail.com"
+    date="12-07-2022"
+    number="2312341243"
+    return render_template("site1.html", name=username,email=email,date=date,number=number ,edit=edit_1)
+
+@app.route("/editProfile")
+def edit_profile():
+    global edit_1
+    edit_1=True
+    print(edit_1)
+    return redirect("/profile")
+
+@app.route("/update")
+def update_profile():
+    global edit_1
+    edit_1=None
+    print(edit_1)
+    return redirect("/profile")
+
 
 if __name__=="__main__" :
     app.run(debug=True)
